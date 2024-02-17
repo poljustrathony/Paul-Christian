9432613d19ec550ca27930349234d37e2c6b6f73
Fajardo0201 <159467653+Fajardo0201@users.noreply.github.com>
2024-02-17 18:54:09 +0800

    Calculator.java

--- Subtract.java
+++ Subtract.java
@@ -1,1 +1,27 @@
 new file Github
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operator;
+
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        double result;
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+            
